e475782c6bab126d2f113b275b91da530e81b6b9
sergioleanmartinez <ser.martineza@duocuc.cl>
2023-10-28 00:41:51 -0300

    "index (css,js), login(css,js) con_alcohol, sin_alcohol

---
+++ Login/login.js
@@ -0,0 +1,16 @@
+
+function validacion(){
+    let user=document.getElementById("usuario").value;
+    let pass=document.getElementById("password").value;
+    if(user == "admin" && pass == "1234"){
+        alert("Usuario correcto")
+        window.location="index2.html";
+        return false;
+    }
+   if(user == "" || pass =="") {
+        alert("falta agregar información")
+    }
+    else{
+        alert("contraseña o usuario invalido")
+    }
+}
